689de5bc2d9b8428fbdbc0b717f3857a466ec714
Mujakkir-Pathan <mujakkir.pathan05@gmail.com>
2026-07-08 15:19:57 +0000

    fixing permission 9

---
+++ system_info.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -euo pipefail
+
+system_info() {
+	echo "===== System Information ====="
+	hostname
+        grep -E '^(NAME|VERSION)=' /etc/os-release
+}
+
+uptime_info() {
+	echo "===== system uptime ====="
+	uptime
+
+}
+
+disk_usage() {
+	echo "===== disk Usage ====="
+	df -h | head -n 6
+         
+
+}
+
+memory_usage() {
+        echo "===== Memory Usage ====="
+	free -h
+         
+}
+
+top_processes() {
+	echo "===== top processes ====="
+        ps aux --sort=-%cpu | head -n 6
+}
+
+main() {
+	 system_info
+	 echo
+
+         uptime_info
+	 echo
+
+         disk_usage
+	 echo
+
+         memory_usage
+	 echo
+
+         top_processes
+	 echo
+}
+
+main
